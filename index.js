0df8fc2ebf00c8ca79a0c9f46869b36667ced18d
poisoned-eden <poisoned.eden@gmail.com>
2023-06-05 21:22:12 +0100

    Adds some questions, and the given challenge files

---
+++ index.js
@@ -0,0 +1,15 @@
+// TODO: Include packages needed for this application
+const markdownify = require("./utils/generateMarkdown.js");
+const questions = require('./utils/questions.js');
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+// set it to ask if like to make new readme or edit what already saved
+// find a way of saving answers as go along - use editor type?
---
+++ utils/questions.js
@@ -0,0 +1,72 @@
+const { default: Choices } = require("inquirer/lib/objects/choices");
+
+const questions = [
+    {
+        type: 'checkbox',
+        name: 'optionalSections',
+        message: 'Which optional sections would you like to include in the ReadMe?',
+        choices: ["Badges", "Features", "How to Contribute", "Tests"],
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of the project?',
+    },
+    {
+        type: 'list',
+        name: 'descriptionType',
+        message: 'What type of description would you like to use?',
+        choices: ['short', 'long'],
+    },
+    {
+        type: 'input',
+        name: 'shortDesc',
+        message: 'Please enter a short description explaining the what, why and how of the project.'
+    },
+    {
+        type: 'input',
+        name: 'descMotivation',
+        message: 'What was your motivation?',
+    },
+    {
+        type: 'input',
+        name: 'descWhy',
+        message: 'Why did you build this project?',
+    },
+    {
+        type: 'input',
+        name: 'descProblem',
+        message: 'What problem does this project solve?',
+    },
+    {
+        type: 'input',
+        name: 'descLearn',
+        message: 'What did you learn whilst building this project?',
+    },
+    {
+        type: 'input',
+        name: 'descStandOut',
+        message: 'What makes your project stand out?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use.',
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'List your collaborators',
+    },
+    {
+        type: 'list',
+        name: 'Licence',
+        message: 'Which licence do you want to use?',
+        choices: ['The MIT Licence', 'The Apache Licence', 'The GPL Licence']
+    },
+];
