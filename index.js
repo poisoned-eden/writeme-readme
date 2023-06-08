070f6dc618869a9a0056cc88d2405c33f3c640c0
poisoned-eden <poisoned.eden@gmail.com>
2023-06-08 22:52:26 +0100

    Revert "removes an unused variable."
    This reverts commit 33289eb18f027c4b79ee0e0b4a265171306cdc23.

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ function print(something) {
     console.log("_______");
 }
 
+var storeResponses = {};
+
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
@@ -54,3 +56,6 @@ async function init() {
 }
 // Function call to initialize app
 init();
+
+// set it to ask if like to make new readme or edit what already saved
+// find a way of saving answers as go along - use editor type inquirer question?
