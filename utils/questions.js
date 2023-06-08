9ae28ba59b3ab3069e774a21e9b675ce254dea8d
poisoned-eden <poisoned.eden@gmail.com>
2023-06-08 18:12:22 +0100

    Organises questions into logical order for different choices

--- utils/questions.js
+++ utils/questions.js
@@ -1,4 +1,9 @@
-const questions = [
+const starterQs = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of the project?',
+    },
     {
         type: 'checkbox',
         name: 'optionalSections',
@@ -6,21 +11,22 @@ const questions = [
         choices: ["Badges", "Features", "How to Contribute", "Tests"],
     },
     {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of the project?',
-    },
-    {
         type: 'list',
         name: 'descriptionType',
         message: 'What type of description would you like to use?',
         choices: ['short', 'long'],
     },
+]
+
+const shortDescQs = [
     {
         type: 'input',
         name: 'shortDesc',
         message: 'Please enter a short description explaining the what, why and how of the project.'
-    },
+    }
+]
+
+const longDescQs = [
     {
         type: 'input',
         name: 'descMotivation',
@@ -45,7 +51,10 @@ const questions = [
         type: 'input',
         name: 'descStandOut',
         message: 'What makes your project stand out?',
-    },
+    }
+]
+
+const requiredQs = [
     {
         type: 'input',
         name: 'installation',
@@ -65,8 +74,57 @@ const questions = [
         type: 'list',
         name: 'Licence',
         message: 'Which licence do you want to use?',
-        choices: ['The MIT Licence', 'The Apache Licence', 'The GPL Licence']
+        choices: ['The MIT Licence', 'The Apache Licence', 'The GPL 3.0 Licence']
     },
-];
+]
 
-module.exports = {questions};
+const badgesQs = [
+    {
+        type: 'checkbox',
+        name: 'badges',
+        message: 'Which badges would you like to add to the ReadMe?',
+        choices: ["HTML", "JavaScript", "CSS", "Profile Badge", "Watch counter"],
+    }
+] 
+
+const featureQ = [
+    {
+        type: 'input',
+        name: 'feature',
+        message: 'Please describe one feature. (You will have the chance to add more)',
+    }
+] 
+
+const anotherFeature = [
+    {
+        type: 'checkbox',
+        name: 'anotherFeature',
+        message: 'Add another feature?',
+        choices: [
+            {name: "yes",
+            checked: true},
+            {name: "no"}
+        ]
+    },
+]
+
+const constributeQs = [
+    {
+        type: 'checkbox',
+        name: 'optionalSections',
+        message: 'Which optional sections would you like to include in the ReadMe?',
+        choices: ["Badges", "Features", "How to Contribute", "Tests"],
+    }
+] 
+const testsQs = [
+    {
+        type: 'checkbox',
+        name: 'optionalSections',
+        message: 'Which optional sections would you like to include in the ReadMe?',
+        choices: ["Badges", "Features", "How to Contribute", "Tests"],
+    }
+] 
+
+
+
+module.exports = {starterQs, shortDescQs, longDescQs, requiredQs, badgesQs, featureQ, anotherFeature, constributeQs, testsQs};
